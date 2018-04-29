8bae52fd7e42269495278f9f8b75f2b058b05d09
Adrian Vovk <adrianvovk@gmail.com>
2018-04-29 15:15:58 -0400

    Bump wayfire

--- wayfire/default.nix
+++ wayfire/default.nix
@@ -7,13 +7,25 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner  = "ammen99";
     repo   = "wayfire";
-    rev    = "edc03e1e423c7cad61102533e011a082c08dc9b8";
-    sha256 = "16l33rcal0h3rywg5bgls88k0my5cz9648cp2mr2xa7ywr2w6bg2";
+    rev    = "d1fb87c9a9b5e8372c57e4cbc8f26005b380e873";
+    sha256 = "010pawk04nir2yyhyjzd4szv1ib584fg3fpn3p3ryjk41jc63wzb";
   };
 
   prePatch = ''
-  	cp ${import ../shell/shell.nix}/wayland-protocols/*.xml proto/
+  	cp ${import ../shell}/wayland-protocols/*.xml proto/
   	substituteInPlace proto/meson.build --replace "/home/ilex/work/wf-basic-deco/proto/wf-decorator.xml" "wf-decorator.xml"
+
+  	# HACK: Make decoration only work with xwayland.
+  	substituteInPlace plugins/decor/decoration.cpp --replace "if (decorator_resource && !decorator.is_decoration_window(view->get_title()))" "if (decorator_resource && !decorator.is_decoration_window(view->get_title()) && wlr_surface_is_xwayland_surface(view->surface))"
+  	substituteInPlace plugins/decor/meson.build --replace "dependencies: [pixman, wf_protos]," "dependencies: [pixman, wf_protos, wlroots],"
+  	substituteInPlace plugins/decor/decoration.cpp --replace '#include "wf-decorator-protocol.h"' "#include \"wf-decorator-protocol.h\"
+  	extern \"C\" {
+  	#define class class_t
+  	#include <wlr/xwayland.h>
+  	#undef class
+  	}"
+
+  	cat plugins/decor/decoration.cpp
   '';
   
   enableParallelBuilding = true;
